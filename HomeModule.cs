42f141f81e07d69e55f97940ee7b9e790d8eefa3
Michael Pollett <michael.pollett@skyscanner.net>
2018-01-24 13:48:06 +0000

    skeleton

---
+++ HomeModule.cs
@@ -0,0 +1,17 @@
+using Nancy;
+namespace NancyApplication
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get("/", args => "Hello World. This is pretty cool!");
+            Get("/test/{name}", args => new Person() { Name = args.name });
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+    }
+}
